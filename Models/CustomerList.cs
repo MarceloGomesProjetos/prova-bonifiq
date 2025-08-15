052520f0f1448dfbd7117ec15964049ce8e88543
Marcelo Gomes <web.marcelo@gmail.com>
2025-08-15 05:23:00 -0300

    Parte2 Controller Resolvido

--- Models/CustomerList.cs
+++ Models/CustomerList.cs
@@ -1,6 +1,5 @@
 ﻿namespace ProvaPub.Models
 {
-	// Replace 'PagedList<Customer>' with the correct generic type or remove type arguments if 'PagedList' is not generic
 	public class CustomerList : PagedList<Customer>
 	{
 	}
